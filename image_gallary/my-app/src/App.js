0b57ddb8c9bf7072535468373857a67304a10b1c
Finn Rea <b9020748@newcastle.ac.uk>
2024-02-21 21:33:08 +0000

    eslint and prettier set up

--- image_gallary/my-app/src/App.js
+++ image_gallary/my-app/src/App.js
@@ -1,18 +1,20 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
-const  App = () => {
-  const [word, setWord] = useState('');
+const App = () => {
+  const [word, setWord] = useState("");
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
 
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
+    )
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -20,15 +22,16 @@ const  App = () => {
       .catch((error) => {
         console.log(error);
       });
-  }
+
+    setWord(""); // Clear the word state
+  };
 
   return (
-    
     <div className="App">
-      <Header title="Images Gallery"/>
-      <Search word={word} setWord = {setWord} handleSubmit={handleSearchSubmit}/>
+      <Header title="Images Gallery" />
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
--- image_gallary/my-app/src/components/header.js
+++ image_gallary/my-app/src/components/header.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { Container, Navbar } from 'react-bootstrap';
+import { Container, Navbar } from "react-bootstrap";
 
 const navbarStyle = {
-    backgroundColor: 'lightblue',
-    color: 'black',
-    marginBottom: '20px'
+  backgroundColor: "lightblue",
+  color: "black",
+  marginBottom: "20px",
 };
 
-
-const Header = ({title}) => {
-    return (
-        <Navbar style={navbarStyle} variant="light">
-            <Container className="justify-content-center">
-                <Navbar.Brand href="/">{title}</Navbar.Brand>
-            </Container>                
-        </Navbar>
-    );
+const Header = ({ title }) => {
+  return (
+    <Navbar style={navbarStyle} variant="light">
+      <Container className="justify-content-center">
+        <Navbar.Brand href="/">{title}</Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Header;
-
