7fc83de49fc37d6953f0abe982f2783150819b2e
Finn Rea <b9020748@newcastle.ac.uk>
2024-02-23 11:29:19 +0000

    update image state

--- image_gallary/my-app/src/App.js
+++ image_gallary/my-app/src/App.js
@@ -7,17 +7,18 @@ const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_ACCESS_KEY;
 
 const App = () => {
   const [word, setWord] = useState("");
+  const [images, setImages] = useState([]);
+  console.log(images);
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    console.log(word);
 
     fetch(
       `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`,
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data);
+        setImages([data, ...images]);
       })
       .catch((error) => {
         console.log(error);
